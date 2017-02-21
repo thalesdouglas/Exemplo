2b5d39ea996c18b032666b16c1668749120e6d68
thales.dsouza <thales.dsouza@CAS511757W0706.senacsp.edu.br>
2017-02-21 20:38:21 -0300

    versão inicial

--- testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
+++ testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
@@ -3,6 +3,8 @@ package br.senac.tads.pi3.testegit;
 public class Principal {
     
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        for (int i = 0; i < 10; i++){
+            System.out.println("Hello World!!" + i + "!!");
+        }
     }
 }
