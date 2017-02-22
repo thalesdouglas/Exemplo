d189da307df0e21034c9b2b082483806e8164753
thales.dsouza <thales.dsouza@CAS511757W0706.senacsp.edu.br>
2017-02-21 21:25:00 -0300

    Mostrar somente se "i" for par

--- testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
+++ testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
@@ -4,7 +4,9 @@ public class Principal {
     
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++){
-            System.out.println("Hello World!!" + i + "!!");
+            if (i % 2 == 0){
+                System.out.println("Hello World!!" + i + "!!");
+            }
         }
     }
 }
